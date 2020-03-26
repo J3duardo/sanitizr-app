ec33245defa6d477b83f2201f270d73b8a70859e
J3duardo <jesus_69eduardo@hotmail.com>
2020-03-26 18:23:54 -0400

    Frontend: Agregado link a página de contacto

--- client/src/components/AppBar.jsx
+++ client/src/components/AppBar.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const NavBar = () => {
   return (
-    <AppBar position="fixed">
+    <AppBar className="app-bar" position="fixed">
       <div style={{width: "100%", maxWidth: "1200px", margin: "0 auto"}}>
         <Toolbar>
-          <Typography variant="h5">
-            <span style={{fontWeight: "700"}}>Sanitizr App</span>
-          </Typography>
+          <IconButton>
+            <Typography variant="h5">
+              <Link to="/">
+                <span style={{fontWeight: 700, color: "#fff"}}>Sanitizr App</span>
+              </Link>
+            </Typography>
+          </IconButton>
+          <Button style={{marginLeft: "auto"}}>
+            <Link to="/contact">
+              <span style={{fontWeight: 400, color: "#fff"}}>Contáctame</span>
+            </Link>
+          </Button>
         </Toolbar>
       </div>
     </AppBar>
